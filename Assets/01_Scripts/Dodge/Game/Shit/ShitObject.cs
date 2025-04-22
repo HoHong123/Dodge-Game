c7649949d223949a32c9fb03a2903a8c945d8d5a
PKH <epzmfhs@gmail.com>
2025-04-22 21:53:24 +0900

    [feat] 🆕 Update Shit object controller.

--- Assets/01_Scripts/Dodge/Game/Shit/ShitObject.cs
+++ Assets/01_Scripts/Dodge/Game/Shit/ShitObject.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
+using Util.CustomAttribute;
+
 
 namespace Dodge.Game.Shit {
-    public class ShitObject : MonoBehaviour {
+    public class ShitObject : MonoBehaviour, IPoolable {
+        [Title("Damage Settings")]
+        [MultiTagSelector("충돌 태그 선택")]
+        [SerializeField]
+        private List<string> validTags = new();
 
+
+        public void OnSpawned() {
+        }
+
+        public void OnReturned() {
+        }
+
+        private void SendDamage() {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision) {
+            var tag = collision.transform.tag;
+            switch (tag) {
+            case "Player": SendDamage(); break;
+            }
+        }
     }
 }
