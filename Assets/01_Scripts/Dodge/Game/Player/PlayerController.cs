fed43cdc86d11bd6bac555f085054000b8b8e147
JasonCatze <epzmfhs@gmail.com>
2025-05-12 18:52:34 +0900

    [feat] 🆕 Update "PlayerController.cs".
    1. Control "HP" slider ui.
    2. Control HP status.

--- Assets/01_Scripts/Dodge/Game/Player/PlayerController.cs
+++ Assets/01_Scripts/Dodge/Game/Player/PlayerController.cs
@@ -1,7 +1,9 @@
-using Sirenix.OdinInspector;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
-
+using Sirenix.OdinInspector;
+using Util.Logger;
 
 namespace Dodge.Game.Player {
     /* Player Controller
@@ -13,11 +15,17 @@ namespace Dodge.Game.Player {
      * 6. Event handling
      * 7. Preset handling
      */
-    public partial class PlayerController : MonoBehaviour {
+    public partial class PlayerController : MonoBehaviour, IDamagable {
+        #region Member
         [Title("Status")]
+        [PropertyRange(0, 100)]
+        [SerializeField]
+        float hp = 100;
         [PropertyRange(0, 10)]
         [SerializeField]
         float moveSpeed = 0;
+
+        Vector2 moveInput;
 
         [Title("Physic")]
         [SerializeField]
@@ -25,8 +33,42 @@ namespace Dodge.Game.Player {
         [SerializeField]
         CapsuleCollider2D collider;
 
-        private Vector2 moveInput;
+        [Title("UI")]
+        [SerializeField]
+        Slider hpSlider;
+        [SerializeField]
+        Animator animation;
+        #endregion
 
+        #region Listener
+        public Action OnStatusChange { get; set; }
+        #endregion
+
+
+        private void Start() {
+            hpSlider.maxValue = hp;
+            hpSlider.value = hp;
+            GameManager.Instance.OnGameOver += _OnGameOver;
+        }
+
+
+        private void FixedUpdate() {
+            r2d.linearVelocity = new Vector2(moveInput.x * moveSpeed, r2d.linearVelocity.y);
+        }
+
+
+        public void TakeDamage(float damage) {
+            HLogger.Log($"Damage :: {damage}");
+            GameManager.Instance.OnScoreChange?.Invoke(-(int)damage);
+
+            if ((hp -= damage) <= 0) {
+                GameManager.Instance.OnGameOver?.Invoke(false);
+            }
+
+            hpSlider.value = hp;
+            animation.SetTrigger("Hit");
+            OnStatusChange?.Invoke();
+        }
 
         public void OnMoveEvent(InputAction.CallbackContext context) {
             moveInput = context.ReadValue<Vector2>();
@@ -36,8 +78,10 @@ namespace Dodge.Game.Player {
             }
         }
 
-        private void FixedUpdate() {
-            r2d.linearVelocity = new Vector2(moveInput.x * moveSpeed, r2d.linearVelocity.y);
+
+        private void _OnGameOver(bool result) {
+            r2d.linearVelocityX = 0;
+            enabled = false;
         }
     }
 }
